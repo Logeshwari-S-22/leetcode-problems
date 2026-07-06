fa63ea02cae152eb187259156a51ac5c5f8ce058
Logeshwari-S-22 <latha4598150@gmail.com>
2026-07-06 19:09:14 +0530

    Create valid parenthesis.cpp

---
+++ valid parenthesis.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+    bool isValid(string s) {
+        stack<int> stk;
+        for(char ch:s){
+            if(ch=='(' || ch=='[' || ch=='{'){
+                stk.push(ch);
+            }
+            else{
+                if(stk.empty()){
+                    return false;
+                }
+                if(ch==')'&&stk.top()=='(' ||
+                   ch==']'&&stk.top()=='[' ||
+                   ch=='}'&&stk.top()=='{'){
+                    stk.pop();
+                   }
+                else{
+                    return false;
+                }
+            }
+        }
+        return stk.empty();
+    }
+};
