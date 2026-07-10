5085f07616ab2960e2d8e98300f31ffa078f9689
Logeshwari-S-22 <latha4598150@gmail.com>
2026-07-10 09:16:24 +0530

    Create Rotate an Array by d - Left.cpp

---
+++ Rotate an Array by d - Left.cpp
@@ -0,0 +1,28 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+void rotateArr(vector<int>& arr, int d) {
+    int n=arr.size();
+    vector<int> temp(n);
+    d=d%n;
+    for(int i=0;i<n-d;i++){
+        temp[i]=arr[d+i];
+    }
+    for(int i=0;i<d;i++){
+        temp[n-d+i]=arr[i];
+    }
+    for(int i=0;i<n;i++){
+        arr[i]=temp[i];
+    }
+}
+
+int main() {
+    vector<int> arr = { 1, 2, 3, 4, 5, 6 };
+    int d = 4;
+
+    rotateArr(arr, d);
+    for (int i = 0; i < arr.size(); i++)
+        cout << arr[i] << " ";
+
+    return 0;
+}
