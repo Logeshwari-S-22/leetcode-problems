5be59c1221460987a4e405755e6aff98241698e6
Logeshwari-S-22 <latha4598150@gmail.com>
2026-07-28 22:48:06 +0530

    Create Subsets.cpp

---
+++ Subsets.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    void func(int idx,vector<int>& nums,vector<int>& ds,vector<vector<int>>& ans){
+        ans.push_back(ds);
+        for(int i=idx;i<nums.size();i++){
+            ds.push_back(nums[i]);
+            func(i+1,nums,ds,ans);
+            ds.pop_back();
+        }
+    }
+    vector<vector<int>> subsets(vector<int>& nums) {
+        vector<vector<int>> ans;
+        vector<int> ds;
+        sort(nums.begin(),nums.end());
+        func(0,nums,ds,ans);
+        return ans;
+    }
+};
