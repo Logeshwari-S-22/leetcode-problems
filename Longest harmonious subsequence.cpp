cfa7c5f60943f4c7aa3b778f0a5b54939a4ea2c7
Logeshwari-S-22 <latha4598150@gmail.com>
2026-08-30 21:43:22 +0530

    Create Longest harmonious subsequence.cpp

---
+++ Longest harmonious subsequence.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    int findLHS(vector<int>& nums) {
+        unordered_map<int,int> mp;
+        for(int x:nums){
+            mp[x]++;
+        }        
+        int ans=0;
+        for(auto it:mp){
+            int x=it.first;
+            if(mp.count(x+1)){
+                ans=max(ans,mp[x]+mp[x+1]);
+            }
+        }
+        return ans;
+    }
+};
